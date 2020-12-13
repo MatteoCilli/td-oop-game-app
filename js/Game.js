67694a22e5c083a6fee32cd7944f22239eed96f1
Matteo Cilli <cilli.matteo@gmail.com>
2020-12-13 17:32:02 +0100

    Strike Package Bravo

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,91 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    createPhrases() {
+        const phrases = [
+            new Phrase('we are going to have peace even if we have to fight for it'),
+            new Phrase('your name is unknown your deed is immortal'),
+            new Phrase('the pope how many divisions has he got'),
+            new Phrase('the frontier of america is on the rhine'),
+            new Phrase('tora tora tora'),
+            new Phrase('if everyone thinks alike someone is not thinking')
+        ];
+        return phrases;
+    }
+
+    startGame() {
+        document.querySelector('#overlay').style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * 5)];
+    }
+
+    // handles onscreen keyboard button clicks
+    handleInteraction(button) {
+        button.disabled = true;
+        if (!this.activePhrase.phrase.includes(button.textContent)) {
+            button.classList = 'wrong';
+            this.removeLife();
+            return;
+        }
+        if (this.activePhrase.phrase.includes(button.textContent)) {
+            button.classList = 'chosen';
+            this.activePhrase.showMatchedLetter(button.textContent);
+        }
+        if (this.checkForWin()) {
+            this.gameOver(true);
+        }
+    }
+
+    removeLife() {
+        this.missed += 1;
+        let gameLives = document.querySelector('img[src="images/liveHeart.png"]');
+        if (this.missed < 5) {
+            gameLives.src = 'images/lostHeart.png';
+        } else if (this.missed === 5) {
+            this.gameOver(false);
+        }
+    }
+
+    gameOver(gameWon) {
+        const gameOverMessage = document.getElementById('game-over-message');
+        const gameOverlay = document.querySelector('#overlay');
+
+        gameOverlay.style.display = 'block';
+        gameOverlay.classList = gameWon ? 'win' : 'lose';
+        gameOverMessage.textContent = gameWon ? 'Battle\'s won! Good Job, Commander!' : 'Temporary retreat is not failure, we shall try again';
+
+        this.resetGame();
+    }
+
+    // did we win?
+    checkForWin() {
+        return document.querySelectorAll('li.hide').length === 0;
+    }
+
+    resetGame() {
+        const letters = document.querySelector('ul');
+        const qwerty = document.querySelectorAll('.keyrow button');
+        const hearts = document.querySelectorAll('#scoreboard img');
+
+        letters.innerHTML = null;
+
+        qwerty.forEach(key => {
+            key.className = 'key';
+            key.disabled = false;
+        });
+
+        hearts.forEach(heart => heart.src = 'images/liveHeart.png');
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+let game;
+
+// starts a new game when clicking the 'Start Game' button
+document.getElementById('btn__reset').addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
+
+// makes '.key' buttons interactive on click
+document.getElementById('qwerty').addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        game.handleInteraction(event.target);
+    }
+});
+
+// on keydown respond to keyboard input, for more interactivity
+
+document.addEventListener('keydown', event => {
+    document.querySelectorAll(`.key`).forEach(element => {
+        if (element.innerHTML === event.key) {
+            if (!element.classList.contains('wrong') && !element.classList.contains('chosen')) {
+                game.handleInteraction(element);
+            }
+        }
+    });
+});
